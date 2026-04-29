4b83ff1b52bae1284481cdca14695b6bb08cb8a4
LAMRANI YASSIR <yassirlamrani07@gmail.com>
2026-04-29 19:03:16 +0100

    init: setup initial de l'application ExamEns

---
+++ src/app/enseignant/enseignant-routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { SaisieNotes } from './saisie-notes/saisie-notes/saisie-notes';
+import { NotesModule } from './notes-module/notes-module/notes-module';  // ← add this
+
+export const enseignantRoutes: Routes = [
+  { path: '',              redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard',    component: Dashboard },
+  { path: 'saisie-notes', component: SaisieNotes },
+  { path: 'notes-module', component: NotesModule },  // ← add this
+];
